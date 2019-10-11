7d90838343003bee026aae9f583690e8dc978d8b
Maddie <maddiemcampbell@gmail.com>
2019-10-11 11:21:05 -0700

    cards styled

--- src/components/cityInfo.jsx
+++ src/components/cityInfo.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 export default class CityInfo extends React.Component {
     render() {
         return (
-             <div style={{marginTop: 20, width: 375, flexBasis: 0}} className="card">
-                    <div className='info-card-title'>
+             <div style={{marginTop: 20, width: 375, flexBasis: 0}} className="card border-info mb-3 card">
+                    <div className='card-header'>
                         <h1 className='title'>City Information</h1>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Location:</div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Humidity: </div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Average temperature: </div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Low temperature:</div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>High temperature: </div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Wind speed: </div>
                     </div>
-                    <div className='list-group-item'>
+                    <div className='list-group-item card-body text-info'>
                         <div className='info'>Air pressure: </div>
                     </div>
                 </div> 
--- src/components/searchHistory.jsx
+++ src/components/searchHistory.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 export default class SearchHistory extends React.Component {
     render() {
         return (
-            <div style={{marginTop: 20, marginRight: 0, width: 375, flexBasis: 0}} className="card">
-                <div className='info-card-title'>
+            <div style={{marginTop: 20, marginRight: 0, width: 375, flexBasis: 0}} className="card border-info mb-3 card">
+                <div className='card-header'>
                     <h1 className='title'>Search Information</h1>
                 </div>
-                <div className='list-group-item'>
+                <div className='list-group-item card-body text-info'>
                     <div className='info'>*City*</div>
                 </div>
-                <div className='list-group-item'>
+                <div className='list-group-item card-body text-info'>
                     <div className='info'>*City*</div>
                 </div>
-                <div className='list-group-item'>
+                <div className='list-group-item card-body text-info'>
                     <div className='info'>*City*</div>
                 </div>
-                <div className='list-group-item'>
+                <div className='list-group-item card-body text-info'>
                     <div className='info'>*City*</div>
                 </div>
-                <div className='list-group-item'>
+                <div className='list-group-item card-body text-info'>
                     <div className='info'>*City*</div>
                 </div>
             </div>
