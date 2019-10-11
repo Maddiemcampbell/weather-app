cdfd1b27801c1a22ae1d97dd90e640dd0f062043
Maddie <maddiemcampbell@gmail.com>
2019-10-11 11:14:41 -0700

    changed widths

--- src/components/searchHistory.jsx
+++ src/components/searchHistory.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class SearchHistory extends React.Component {
     render() {
         return (
-            <div style={{marginTop: 20, marginRight: 0, width: 350, flexBasis: 0}} className="card">
+            <div style={{marginTop: 20, marginRight: 0, width: 375, flexBasis: 0}} className="card">
                 <div className='info-card-title'>
                     <h1 className='title'>Search Information</h1>
                 </div>
