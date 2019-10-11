26ab456909fb35a10990164ea8fb16eb3c8206c6
Maddie <maddiemcampbell@gmail.com>
2019-10-11 09:40:53 -0700

    styling for header

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -8,10 +8,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Header />
-        <SearchBar />
-        <CityInfo />
-        <SearchHistory />
+        <div className='jumbotron'>
+          <Header />
+        </div>
+        <div className='row'>
+          <SearchBar />
+        </div>
+        <div className='col-12 md-6 mb-4'>
+          <CityInfo />
+        </div>
+        <div className='col-12 md-6 mb-4'>
+          <SearchHistory />
+        </div>
       </div>
     );
   }
