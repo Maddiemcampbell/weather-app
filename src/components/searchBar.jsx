54d8c4956475321188e9a52097045cd9eb2a516b
Maddie <maddiemcampbell@gmail.com>
2019-10-11 11:13:00 -0700

    static app done

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <label>Find Your City's Weather!</label>
-        <input placeholder='type in your city here' className="input-group mb-3"></input>
-        <button className='btn btn-info'>Find the weather!</button>
+      <div className='container' style={{marginLeft: 10}}>
+        <label style={{fontSize: 20}}>Find Your City's Weather!</label>
+
+        <div style={{display: "flex"}}>
+          <input style={{width: 600, marginRight: 40}} placeholder='type in your city here' className="input-group mb-3"></input>
+          <button className='btn btn-info'>Find the weather!</button>
+        </div>
       </div>
     );
   }
