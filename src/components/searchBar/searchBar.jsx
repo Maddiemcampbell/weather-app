82d2b5c74ed718ba29ea6b209e9ea2c5c125c9c8
Maddie <maddiemcampbell@gmail.com>
2019-10-12 10:59:09 -0700

    handle submit function working on search click, only console logging right now

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      city: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ city: e.target.value });
+  }
+
+  handleSubmit() {
+    console.log(this.state.city, "handle Submit function working")
+  }
+
   render() {
     return (
       <div className='container' style={{marginLeft: 10}}>
         <label style={{fontSize: 20}}>Find Your City's Weather!</label>
 
         <div style={{display: "flex"}}>
-          <input style={{width: 600, marginRight: 40}} placeholder='type in your city here' className="input-group mb-3"></input>
-          <button className='btn btn-info'>Find the weather!</button>
+          <input onChange={this.handleChange} value={this.state.city} style={{width: 600, marginRight: 40}} placeholder='type in your city here' className="input-group mb-3"></input>
+          <button className='btn btn-info' onClick={this.handleSubmit}>Find the weather!</button>
         </div>
       </div>
     );
--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import CityInfo from '../components/cityInfo';
-import Header from '../components/header';
-import SearchBar from '../components/searchBar';
-import SearchHistory from '../components/searchHistory';
+import CityInfo from '../components/cityInfo/cityInfo';
+import Header from '../components/header/header';
+import SearchBar from '../components/searchBar/searchBar';
+import SearchHistory from '../components/searchHistory/searchHistory';
 
 export default class App extends React.Component {
   render() {
